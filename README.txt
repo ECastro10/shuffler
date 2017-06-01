3dbe338b66ebc05a26046acce53c851973fc8e45
Emanuel <eecastro1011@gmail.com>
2017-06-01 16:39:28 -0700

    finished the tool, now it successfully shuffles a list of items

--- README.txt
+++ README.txt
@@ -1,2 +1,15 @@
 The idea behind shuffler is simple: input items and create a list that can be shuffled.
+
 It originally started out as an idea to shuffle book titles but I soon realized that it can be applied to anything, this is why the project folder is called book_shuffler.
+
+These are the following requirements:
+Django (1.11.2)
+pip (9.0.1)
+pytz (2017.2)
+setuptools (36.0.1)
+wheel (0.29.0)
+
+Feel free to use it, it doesn't protect handle XSS because it is just a simple tool that I created for a simple purpose.
+I did borrow 4 lines of code that are based on Fisher-Yates shuffle provided Alexey Lebedev on Stack Overflow.
+
+
--- static/shuffler_scripts.js
+++ static/shuffler_scripts.js
@@ -6,15 +6,23 @@ function addToList(){
     var item = $('#input_area').val();
 
     $('#notifier').html("Added that shit!");
-    $('#notifier').fadeIn(700);
-    $('#notifier').fadeOut(500);
+    $('#notifier').fadeIn(500);
+    $('#notifier').fadeOut(300);
 
     $('#malleable_list').append('<li>' + item + '</li>');
     $('#input_area').val("");
 }
 
 function shuffleThatShit(){
-    var itemsToShuffle = $('malleable_list');
 
-    console.log(itemsToShuffle[0])
+
+    //The following 4 lines are based on Fisher-Yates shuffle provided Alexey Lebedev on Stack Overflow
+    var ul = document.querySelector('ul');
+    for (var i = ul.children.length; i >= 0; i--){
+        ul.appendChild(ul.children[Math.random() * i | 0])
+    }
+
+    $('#notifier').html("Shuffled!");
+    $('#notifier').fadeIn(500);
+    $('#notifier').fadeOut(300);
 }
